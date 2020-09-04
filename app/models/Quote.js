91e569b94268be59fb740c99cfbac2f7a73b95d9
Lawrence Haney <Lawrence.r.haney@gmail.com>
2020-09-04 17:01:40 -0600

    its alive, and i hate it

--- app/models/Quote.js
+++ app/models/Quote.js
@@ -7,7 +7,8 @@ export default class Quote {
   }
 
   get Template(){
-return `<h1 class="text-center text-light">${this.body}</h1>`
+return `<h1 class="text-center text-light text-shadow quote">${this.body}</h1>
+        <h6 class="hide text-center text-light text-shadow">${this.author}</h6>`
   }
 
 }
